2c5f1b84f5c8f7bd32daec977e93a7932db73b66
Jamie Gunn <1107484g@AD.DCS.GLA.AC.UK>
2018-07-10 16:37:47 +0100

    Threading added

---
+++ project work/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Project work',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='1107484g',
+    author_email='',
+    description=''
+)
